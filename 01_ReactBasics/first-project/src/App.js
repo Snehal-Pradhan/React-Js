e5fafa04da660286325f197b0c8d0fa38c2b79b1
Snehal Pradhan <snehalpradhan23@gmail.com>
2024-09-01 12:13:59 +0000

    new start

--- 01_ReactBasics/first-project/src/App.js
+++
@@ -1,6 +0,0 @@
-function App() {
-  return (
-    <h1>Hello World,by snehal</h1>
-  )}
-
-export default App;
--- 01_ReactBasicsVite/first_vite_react/src/App.jsx
+++
@@ -1,9 +0,0 @@
-
-function App() {
-
-  return (
-    <h1>Hello World, By snehal</h1>
-  )
-}
-
-export default App
